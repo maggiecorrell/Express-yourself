19748c39ad18bb3ad189ae7c8c90400046cf6138
Maggie Correll <maggie.correll@gmail.com>
2016-08-18 08:37:27 -0400

    added more passing tests"

--- text-miner/textminer/separator.py
+++ text-miner/textminer/separator.py
@@ -10,9 +10,28 @@ def phone_number(x):
         area_code, num1, num2 = match.groups()
         return {"area_code": area_code, "number": num1 + '-' + num2}
 
+#
+# def money(x):
+#     match = re.search(r'^\$\d{1,}(,\d{3})*[.]?(\d{2})?$', x)
+#     if match:
+#         currency, amount = match.groups()
+#         return {"currency": currency, "number": amount}
 
-def money(x):
-    match = re.search(r'^\$\d{1,}(,\d{3})*[.]?(\d{2})?$', x)
+
+def zipcode(x):
+    match = re.search(r'^(\d{5})\-?(\d{4})?$', x)
     if match:
-        currency, amount = match.groups()
-        return {"currency": currency, "number": amount}
+        zip, plus4 = match.groups()
+        return {"zip": zip, "plus4": plus4}
+
+
+def date(x):
+    date_regex = [r'(\d{1,2})/(\d{1,2})/(\d{4}|\d{2})',
+                  r'(\d{4})-?(\d{2})-?(\d{2})',
+                  r'(\d{1,2})\s*([A-Za-z])\s*(\d{4})',
+                  r'([A-Za-z]{3})\s*(\d{1,2})\s*,?\s*(\d{4})']
+    for regex in date_regex:
+        match = re.search(regex, x)
+        if match:
+            month, day, year = match.groups()
+            return {"month": month, "day": day, "year": year}
